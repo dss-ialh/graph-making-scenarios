6c6b228700bba96628b03a3ceedd79b8bb7a899e
Andriy V. Koval <koval.andrey@gmail.com>
2019-06-03 20:37:21 -0700

    phases 5 and 6 stabel

--- analysis/scenario-3/scenario-3-ccdss.R
+++ analysis/scenario-3/scenario-3-ccdss.R
@@ -261,7 +261,7 @@ g2d <- ds1 %>%
 
 # ---- phase-2-make_plot-1 --------------------------
 # now let us re-express this plot as a custom function
-make_plot_1 <- function(
+make_plot_1_basic <- function(
   d
   ,measure = "rate"
 ){
@@ -287,7 +287,7 @@ make_plot_1 <- function(
 ds1 %>% 
   dplyr::filter(sex       %in% c("Males","Females") ) %>% 
   # notice that we keep operations on the data outside of the function definition
-  make_plot_1(measure = "rate")
+  make_plot_1_basic(measure = "rate")
 
 # ---- phase-2-make_plot-2 --------------------------
 # We need our function to offer us a convenient way to:
@@ -371,47 +371,28 @@ ds1 %>%
 # isolate the preparatory operations from the `make_plot` function
 prep_data_plot_1 <- function(
   d_input
-  ,set_area      = c("Canada")
-  ,set_age_group = c("20-34")
-  ,set_sex       = c("Males","Females")
+  ,set_area      #= c("Canada")
+  ,set_age_group #= c("20-34")
+  ,set_sex       #= c("Males","Females")
 ){
   d1 <- d_input  # for within-function use
-  # d1 <- ds1  # for testing and development
-  
-  # create support objects
-  order_of_age_groups <- d1 %>% 
-    dplyr::arrange() %>% 
-    dplyr::distinct(age_group) %>% 
-    as.list() %>% unlist() %>% as.character()
-  # to position the total value at the END of the vector
-  order_of_age_groups <- c(setdiff(order_of_age_groups,"1+"),"1+")
-  
-  order_of_areas <- d1 %>% 
-    dplyr::distinct(area) %>% 
-    dplyr::arrange(area) %>% 
-    as.list() %>% unlist() %>% as.character()
-  # to position the total value at the BEGINNING of the vector
-  order_of_areas <- c("Canada", setdiff(order_of_areas, "Canada")  )
-
-  # to customize the order of levels
-  levels_sex <- c("Females", "Males","Both sexes")
   
   d2 <- d1 %>% 
-    dplyr::filter(area      %in% c(set_area)     ) %>%
-    dplyr::filter(age_group %in% c(set_age_group))%>%
-    dplyr::filter(sex       %in% c(set_sex)  ) %>% 
+    dplyr::filter(area      %in% set_area      ) %>%
+    dplyr::filter(age_group %in% set_age_group ) %>%
+    dplyr::filter(sex       %in% set_sex       ) %>% 
     dplyr::mutate(
       # to create a shorter label
       years_since_2000 = year - 2000
       # to enforce the chosen order of the levels:
-      ,area      = factor(area,      levels = order_of_areas)
-      ,age_group = factor(age_group, levels = order_of_age_groups)
-      ,sex       = factor(sex,       levels = levels_sex )
+      ,area      = factor(area,      levels = set_area)
+      ,age_group = factor(age_group, levels = set_age_group)
+      ,sex       = factor(sex,       levels = set_sex )
     )
   # to store objects for passing to the `make_plot` function
   l_support <- list()
   l_support[["data"]] <- d2
-  l_support[["levels"]][["sex"]]    <- levels_sex
+  l_support[["set"]] <- list() # in case the first element is single
   l_support[["set"]][["sex"]]       <- set_sex
   l_support[["set"]][["area"]]      <- set_area
   l_support[["set"]][["age_group"]] <- set_age_group
@@ -423,9 +404,12 @@ prep_data_plot_1 <- function(
 # how to use
 l_support <- ds1 %>% 
   prep_data_plot_1(
-     set_sex       = c("Females", "Males") 
-    ,set_area      = c("Canada", "Alberta", "British Columbia") 
-    ,set_age_group = c("1-19", "20-34", "80+","1+")
+    set_area      = c("Canada")
+    # set_area      = c("Canada", "Alberta", "British Columbia") 
+    # ,set_sex       = c("Females", "Males")
+    ,set_sex       = c("Both sexes")
+    # ,set_age_group = c("1-19", "20-34", "80+","1+")
+    ,set_age_group = c("1+")
   )
 l_support %>% print()
 
@@ -457,17 +441,20 @@ make_plot_1 <- function(
     scale_color_manual(values = palette_sex_dark)+
     # scale_color_manual(values = pallete_sex_light)+
     theme_minimal()+
-    labs( title = "Crude prevalence of MH service utilization in BC among 20-34 year olds")
-  l_support[["graph"]] <- g_out
+    labs( title = "Crude prevalence of MH service utilization")
+  l_support[["graph"]]   <- g_out
   l_support[["measure"]] <- measure
   return(l_support)  
 }
 # how to use
 l_support <- ds1 %>% 
   prep_data_plot_1(
-    set_sex        = c("Females", "Males", "Both sexes") 
-    ,set_area      = c("Canada", "Alberta", "British Columbia") 
-    ,set_age_group = c("1-19", "20-34", "80+","1+")
+    # set_area      = c("Canada")
+    set_area      = c("Canada", "Alberta", "British Columbia")
+    ,set_sex       = c("Females", "Males")
+    # ,set_sex       = c("Both sexes")
+    # ,set_age_group = c("1-19", "20-34", "80+","1+")
+    ,set_age_group = c("1+")
   ) %>% 
   make_plot_1(measure = "rate")
 l_support$graph %>% print()
@@ -522,7 +509,8 @@ print_plot_1 <- function(
 # how to use
 l_support <- ds1 %>% 
   prep_data_plot_1(
-    set_sex        = c("Females", "Males", "Both sexes") 
+    set_sex        = c("Females", "Males") 
+    # set_sex        = c("Females", "Males", "Both sexes") 
     ,set_area      = c("Canada", "Alberta", "British Columbia") 
     ,set_age_group = c("1-19", "20-34", "80+","1+")
   ) %>% 
@@ -540,18 +528,20 @@ l_support <- ds1 %>%
     ,quality = 100
     ,res     = 200
   )
+# ---- phase-4-print_plot-1 ---------------------------------
 # notice that if I print the GRAPH by reaching into the `l_support` object
 # if will be displayed according to the `fig.width`, `fig.height`, and `out.width`
 # parameters specified in the chuck options (in the .Rmd file)
 l_support$graph %>% print()
 
+# ---- phase-4-print_plot-2 ---------------------------------
 # if, however, we reach into the disk, we will recover the image generated
 # with the dimensions and specs defined in the `print_plot` function
-l_support$path_plot
 l_support$path_plot %>% jpeg::readJPEG() %>% grid::grid.raster()
 
 
-# ---- ----------------------
+
+# ---- dimensions ----------------------
 # canvas size guide ( portrait orientation )
 
 # Size           Width x Height (mm) Width x Height (in)  Aspect Ratio
@@ -563,11 +553,69 @@ l_support$path_plot %>% jpeg::readJPEG() %>% grid::grid.raster()
 
 
 # ---- phase-5-serialize ---------------------------------
-# 
+# it often makes sense to genrate a series of plot to be explored manually
+
+# GRAPH SERIES 1
+path_target <- "./analysis/scenario-3/prints/series_1/"
+provinces_to_pair     <- c("British Columbia", "Alberta", "Quebec")
+age_groups_to_display <- c("1-19", "20-34", "35-49", "50-64","65-79","80+","+1")
+# for each selected province create a comparison with Canada
+ls_plot_series <- list()
+for(province_i in provinces_to_pair){
+  ls_plot_series[[province_i]]  <- ds1 %>%
+    prep_data_plot_1(
+      set_sex        = c("Females", "Males")
+      ,set_area      = c("Canada", province_i)
+      ,set_age_group = age_groups_to_display
+    ) %>%
+    make_plot_1(
+      measure = "rate"
+    ) %>%
+    print_plot_1(
+      path_output_folder = path_target
+      # options added through `...` into the jpeg() function
+      ,width   = 1700
+      ,height  = 500
+      ,units   = "px"
+      ,quality = 100
+      ,res     = 200
+    )
+}
+saveRDS(ls_plot_series, paste0(path_target,"ls_plots.rds") )
+
+# GRAPH SERIES 2
+path_target <- "./analysis/scenario-3/prints/series_2/"
+provinces_to_pair    <- c("British Columbia", "Alberta", "Quebec")
+age_groups_to_display <- c( "+1")
+# for each selected province create a comparison with Canada
+ls_plot_series <- list()
+for(province_i in provinces_to_pair){
+  ls_plot_series[[province_i]]  <- ds1 %>%
+    prep_data_plot_1(
+      set_sex        = c("Males","Females")
+      ,set_area      = c("Canada", province_i)
+      ,set_age_group = c("20-34", "+1")
+    ) %>%
+    make_plot_1(
+      measure = "rate"
+    ) %>%
+    print_plot_1(
+      path_output_folder = path_target
+      # options added through `...` into the jpeg() function
+      ,width   = 900
+      ,height  = 500
+      ,units   = "px"
+      ,quality = 100
+      ,res     = 200
+    )
+}
+saveRDS(ls_plot_series, paste0(path_target,"ls_plots.rds") )
+
 
 # ---- phase-6-place_plot ---------------------------------
-l_support$path_plot
-l_support$path_plot %>% jpeg::readJPEG() %>% grid::grid.raster()
+l_support <- readRDS("./analysis/scenario-3/prints/series_1/ls_plots.rds")
+l_support[["British Columbia"]]$path_plot
+l_support[["British Columbia"]]$path_plot %>% jpeg::readJPEG() %>% grid::grid.raster()
 
 # ---- publish ---------------------------------------
 # This chunk will publish the summative report
